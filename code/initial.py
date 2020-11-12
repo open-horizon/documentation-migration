703f2b6407c60a40860efb1ac0c2f9a702b0e948
Edidiong Etuk <edidiongmichael08@gmail.com>
2020-11-12 13:17:19 +0100

    build the docs into the present repo docs folder
    Signed-off-by: Edidiong Etuk <edidiongmichael08@gmail.com>

--- code/initial.py
+++ code/initial.py
@@ -1,9 +1,8 @@
 from  shutil import ignore_patterns, copytree
 
-
 ignore_list = [".git", "images", "quick_start", "nl", "api", "cli", "tools"]
 
-def oh_folders():
+def oh_folders(src, dest):
     src = "/home/edeediong/Documents/Open Source/docs"
-    dest = "/home/edeediong/transpose/"
-    copytree(src, dest, ignore=ignore_patterns(*ignore_list))
+    dest = "docs/"
+    copytree(src, dest, ignore=ignore_patterns(*ignore_list), dirs_exist_ok=True)
