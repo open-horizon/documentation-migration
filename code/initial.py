cee76e85a529c8dbf706237853ae6d727f1831b9
Edidiong Etuk <edidiongmichael08@gmail.com>
2020-11-13 02:42:17 +0100

    feat: Remove references to ignore_list
    Signed-off-by: Edidiong Etuk <edidiongmichael08@gmail.com>

--- code/initial.py
+++ code/initial.py
@@ -2,7 +2,16 @@ from  shutil import ignore_patterns, copytree
 
 ignore_list = [".git", "images", "quick_start", "nl", "api", "cli", "tools"]
 
-def oh_folders(src, dest):
+def oh_folders(*args):
     src = "/home/edeediong/Documents/Open Source/docs"
     dest = "docs/"
     copytree(src, dest, ignore=ignore_patterns(*ignore_list), dirs_exist_ok=True)
+
+
+with open("docs/SUMMARY.md", "r+") as opened_file:
+    summary = opened_file.readlines()
+
+    for line in summary:
+        if any(ext in line for ext in ignore_list):
+            print(line)
+            summary.remove(line)
