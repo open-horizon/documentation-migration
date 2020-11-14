ea9de7b98fbeb8f703ed98c357180ccc5b9c2c37
Edidiong Etuk <edidiongmichael08@gmail.com>
2020-11-14 21:45:24 +0100

    Documented the code
    Signed-off-by: Edidiong Etuk <edidiongmichael08@gmail.com>

--- code/initial.py
+++ code/initial.py
@@ -3,17 +3,21 @@ from  shutil import ignore_patterns, copytree
 ignore_list = [".git", "images", "quick_start", "nl", "api", "cli", "tools"]
 
 def oh_folders(*args):
+    """Copies the folder from the source directory into a docs folder eliminating commercial information."""
     src = "/home/edeediong/Documents/Open Source/docs"
     dest = "docs/"
     copytree(src, dest, ignore=ignore_patterns(*ignore_list), dirs_exist_ok=True)
 
+def edit_summary():
+    """Remove references to elements in the ignore list."""
+    with open("docs/SUMMARY.md", "r") as opened_file:
+        summary = opened_file.readlines()
 
-oh_folders()
+    with open("docs/SUMMARY.md", "w") as opened_file:
+        for line in summary:
+            if not any(ext in line for ext in ignore_list):
+                opened_file.write(line)
 
-with open("docs/SUMMARY.md", "r") as opened_file:
-    summary = opened_file.readlines()
-
-with open("docs/SUMMARY.md", "w") as opened_file:
-    for line in summary:
-        if not any(ext in line for ext in ignore_list):
-            opened_file.write(line)
+if __name__ == '__main__':
+    oh_folders()
+    edit_summary()
