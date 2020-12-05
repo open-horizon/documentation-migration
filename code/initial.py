2279f0fc1f72f0c6dabffcbe29c86d4a686738d5
Edidiong Etuk <edidiongmichael08@gmail.com>
2020-12-05 01:15:34 +0100

    Issue8 (#9)
    * feat: initial code for updating dates
    
    Signed-off-by: Edidiong Etuk <edidiongmichael08@gmail.com>
    
    * feat: Introducing updated date
    
    * refactor: added comments to understand execution better
    
    Signed-off-by: Edidiong Etuk <edidiongmichael08@gmail.com>

--- code/initial.py
+++ code/initial.py
@@ -1,6 +1,8 @@
-from  shutil import ignore_patterns, copytree
+from shutil import ignore_patterns, copytree
+from datetime import datetime, date
+import os
 
-ignore_list = [".git", "images", "quick_start", "nl", "api", "cli", "tools"]
+ignore_list = [".git", "quick_start", "nl", "api", "cli", "tools", "integration.md", "offline_installation.md", "conrefs.yml"]
 
 def oh_folders(*args):
     """Copies the folder from the source directory into a docs folder eliminating commercial information."""
@@ -18,6 +20,21 @@ def edit_summary():
             if not any(ext in line for ext in ignore_list):
                 opened_file.write(line)
 
+def update_date():
+    directory = "docs/"
+    for filename in os.listdir(directory):
+        if filename.endswith(".md"):
+            a_file = open(filename, "r") #open the file and read
+            content = a_file.readlines()
+
+            content[3] = f"years: {datetime.now().year}\n"
+            content[4] = f'lastupdated: "{date.today()}"\n'
+            a_file = open(filename, "w") #open the same file and overrite line3 & 4
+            a_file.writelines(content)
+
+            a_file.close()
+
 if __name__ == '__main__':
     oh_folders()
     edit_summary()
+    update_date()
