6ded8edd923b1b74aa9ff10d6fab5d588c8e8f5e
Edidiong Etuk <edidiongmichael08@gmail.com>
2020-12-08 15:51:18 +0100

    refactor: Fixed editing svg files bug
    Signed-off-by: Edidiong Etuk <edidiongmichael08@gmail.com>

--- code/initial.py
+++ code/initial.py
@@ -1,6 +1,8 @@
-from  shutil import ignore_patterns, copytree
+from shutil import ignore_patterns, copytree
+from datetime import datetime, date
+import os
 
-ignore_list = [".git", "images", "quick_start", "nl", "api", "cli", "tools"]
+ignore_list = [".git", "quick_start", "nl", "api", "cli", "tools", "integration.md", "offline_installation.md", "conrefs.yml"]
 
 def oh_folders(*args):
     """Copies the folder from the source directory into a docs folder eliminating commercial information."""
@@ -18,6 +20,26 @@ def edit_summary():
             if not any(ext in line for ext in ignore_list):
                 opened_file.write(line)
 
+def update_date():
+    """This function loops through directory and updates dates of files in said directory."""
+    directory = "docs/"
+    for root, dirs, files in os.walk(directory):
+        ignore = ["README.md","SUMMARY.md"]
+        file_list = [edit_files(root + "/" + file) for file in files if (file not in ignore and file.endswith(".md"))]
+
+def edit_files(i_file):
+    """This function updates date of files passed into it."""
+    a_file = open(i_file, "r")
+    content = a_file.readlines()
+
+    content[3] = f"years: {datetime.now().year}\n"
+    content[4] = f'lastupdated: "{date.today()}"\n'
+    a_file = open(i_file, "w") #open the same file and overrite line3 & 4
+    a_file.writelines(content)
+
+    a_file.close()
+
 if __name__ == '__main__':
     oh_folders()
     edit_summary()
+    update_date()
